72284730fb4cbca3bc3a65ad54bc966528b7b0b6
BRCMOURA <brcmoura@gmail.com>
2024-10-04 21:15:51 -0300

    Add files via upload

---
+++ Exercício7.py
@@ -0,0 +1,9 @@
+#Média de três números
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+n3 = int(input("Digite o terceiro número: "))
+
+media = (n1 + n2 + n3) / 3
+
+print("A média dos três números é igual a: ", media)
